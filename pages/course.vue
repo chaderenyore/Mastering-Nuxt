1ad5ecce17246b2c20311dd0dc42eeffb9631e54
Nyore Chadere <chaderenyore480@gmail.com>
2023-05-19 22:59:10 +0100

    Feat - Navigating with Nuxt link

--- pages/course.vue
+++ pages/course.vue
@@ -16,26 +16,27 @@
         class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
       >
         <h3>Chapters</h3>
-        <!-- <div
-					class="space-y-1 mb-4 flex flex-col"
-					v-for="chapter in chapters"
-					:key="chapter.slug"
-				>
-					<h4>{{ chapter.title }}</h4>
-					<NuxtLink
-						v-for="(lesson, index) in chapter.lessons"
-						:key="lesson.slug"
-						class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
-						:to="lesson.path"
-						:class="{
-							'text-blue-500': lesson.path === $route.fullPath,
-							'text-gray-600': lesson.path !== $route.fullPath,
-						}"
-					>
-						<span class="text-gray-500">{{ index + 1 }}.</span>
-						<span>{{ lesson.title }}</span>
-					</NuxtLink>
-				</div> -->
+        <div
+          class="space-y-1 mb-4 flex flex-col"
+          v-for="chapter in chapters"
+          :key="chapter.slug"
+        >
+          <h4>{{ chapter.title }}</h4>
+
+          <NuxtLink
+            v-for="(lesson, index) in chapter.lessons"
+            :key="lesson.slug"
+            class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
+            :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
+            :class="{
+              'text-blue-500': lesson.path === $route.fullPath,
+              'text-gray-600': lesson.path !== $route.fullPath,
+            }"
+          >
+            <span class="text-gray-500">{{ index + 1 }}.</span>
+            <span>{{ lesson.title }}</span>
+          </NuxtLink>
+        </div>
         <!-- All the lessons and chapters listed here -->
       </div>
 
@@ -45,7 +46,7 @@
     </div>
   </div>
 </template>
-<!-- 
+
 <script setup>
-const { chapters } = useCourse();
-</script> -->
+  const { chapters } = useCourse();
+</script>
