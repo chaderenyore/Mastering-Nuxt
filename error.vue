71276f00945bb7d0826111a0e00358dc9bbae676
Nyore Chadere <chaderenyore480@gmail.com>
2023-05-30 22:49:03 +0100

    Feat - adding a 404 error page

---
+++ error.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="prose">
+    <div v-if="props.error.statusCode === 404">
+       <h1>404</h1>
+       <p>I guess this page doesn't exist</p>
+    </div>
+   </div>
+</template>
+
+<script setup>
+   const props = defineProps({
+      error: Object
+   });
+console.log(props.error);
+const handleError = () => {
+   clearError({ redirect: '/' })
+}
+
+definePageMeta({
+   layout: 'default'
+})
+</script>
